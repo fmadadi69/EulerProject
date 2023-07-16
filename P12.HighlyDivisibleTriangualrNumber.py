255eba5bb78d5487002309f40fae31229b3dfa02
Faezeh <fmadadi69@gmail.com>
2023-07-16 10:50:18 +0330

    Euler problem 12 is modified to improve Time complexity.

--- P12.HighlyDivisibleTriangualrNumber.py
+++ P12.HighlyDivisibleTriangualrNumber.py
@@ -1,3 +1,4 @@
+import numpy as np
 def make_triangular_num(n):
     a = n * (n + 1) / 2
     return int(a)
@@ -5,19 +6,49 @@ def make_triangular_num(n):
 
 # print(make_triangular_num(7))
 
-def count_divisibles_of_num(num):
-    total = sum(1 for k in range(1, num + 1) if num % k == 0)
-    return total
+# def count_divisibles_of_num(num):
+
+def isprime(num):
+    for i in range(2, int(num ** 2) + 1):
+        if num % i == 0:
+            return False
+    else:
+        return True
 
 
+def nextprime(num):
+    num += 1
+    while isprime(num):
+        num += 1
+    return num
+
+
+def prime_factors(num):
+    primeFactors = []
+    prime_factors_dict = dict()
+    i = 2
+    while (num > 1):
+        if num % i == 0:
+            primeFactors.append(i)
+            num = num / i
+        else:
+            i = nextprime(i)
+    for item in set(primeFactors):
+        prime_factors_dict[item] = primeFactors.count(item)
+
+    return prime_factors_dict
+
+
+print(prime_factors(28).values())
+print(np.prod([i+1 for i in list(prime_factors(28).values())]))
 
 
 def triangular_divisibles_pair(to_num):
     mydict = {}
-    mydict[make_triangular_num(1)] = count_divisibles_of_num(make_triangular_num(1))
+    mydict[make_triangular_num(1)] =np.prod([i+1 for i in prime_factors(1).values()])
     n = 2
     while list(mydict.values())[-1] < to_num:
-        mydict[make_triangular_num(n)] = count_divisibles_of_num(make_triangular_num(n))
+        mydict[make_triangular_num(n)] =np.prod([i+1 for i in prime_factors(make_triangular_num(n)).values()])
         n += 1
     return list(mydict.items())[-1]
 
