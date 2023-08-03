c600f7cefc8c259303985cad1b7eacc9eb4ae27a
faezeh <fmadadi69@gmail.com>
2023-08-03 10:19:12 +0330

    Second solution is added to problem 7.

--- P07.10001stPrime.py
+++ P07.10001stPrime.py
@@ -1,3 +1,4 @@
+# Solution number 1 #####################################
 def is_prime(num):
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
@@ -23,3 +24,21 @@ def nth_prime(index):
 
 
 print(nth_prime(10001))
+
+# Solution number 2 #####################################
+def sieve(n):
+    is_prime1 = [True] * n
+    is_prime1[0] = False
+    is_prime1[1] = False
+    for i in range(2, int(n ** 0.5 + 1)):
+        index = i * 2
+        while index < n:
+            is_prime1[index] = False
+            index = index + i
+    prime = []
+    for i in range(n):
+        if is_prime1[i]:
+            prime.append(i)
+    return prime
+
+print(sieve(1001)[-1])
