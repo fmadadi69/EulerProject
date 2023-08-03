d7f633ad0d268d7c43181c4c12a852a010c17f81
faezeh <fmadadi69@gmail.com>
2023-08-03 10:18:22 +0330

    Euler Problem 26 is solved.

--- P26.ReciprocalCycles.py
+++ P26.ReciprocalCycles.py
@@ -1,3 +1,13 @@
+"""
+Mathematics Tips of this problem:
+1. The maximum repeating decimals of number n is 'n-1'
+2. If number n is prime, its repeating decimal length can be calculated by RECURRING DECIMAL function below
+3. If number n is coprime to 10, number n can be written as n=a*b, the length of repeating decimal is equal to the
+    LCM of number of repeating decimals of a , b
+4. If number n is not coprime to 10, and after prime factorization it can be written as n = 2^a * 5^b * p^c * q^d,
+    where p,q are prime numbers and a,b,c,d are the 2,3,p,q, the number of repeating decimal of n is equal to the
+    number of repeating decimals p^c * q^d
+"""
 def recurring_decimal(divisor):
     if divisor < 10:
         divident = 10
@@ -22,9 +32,6 @@ def recurring_decimal(divisor):
                     recurring_seg += '0'
         if divident == value_to_check:
             return recurring_seg
-
-
-#print(recurring_decimal(3))
 
 
 def is_prime(num):
@@ -53,9 +60,6 @@ def prime_nums(to_num):
     return prime_list
 
 
-print(prime_nums(100))
-
-
 def gcd(n1, n2):
     remainder = 1
     if n1 < n2:
@@ -67,31 +71,28 @@ def gcd(n1, n2):
     return n1
 
 
-#print(gcd(18, 36))
-
-
 def lcm(n1, n2):
-    return n1 * n2 / gcd(n1, n2)
+    return (n1 * n2) / gcd(n1, n2)
 
 
 primes = prime_nums(1000)
-d = {i: 0 for i in range(1, 1000)}
-print(primes[0])
 
+d = {i: 0 for i in range(1, 1000)}
 d[3] = 1
+
 for i in primes[3:]:
-    d[i] = len(recurring_decimal(i))
+    d[i] = len(recurring_decimal(i)) # The recurring decimals of prime numbers
 
 for i in range(6, 1000):
     if not d[i]:
-        if i % 2 != 0 and i % 5 != 0:
+        if i % 2 != 0 and i % 5 != 0: # The recurring decimals of numbers which are coprime to 10
             for j in primes:
                 if i % j == 0:
-                    n1 = j
-                    n2 = i / j
+                    n1 = d[j] # The recurring decimals of the prime number which is divisor to i
+                    n2 = d[i / j] # The recurring decimals of i/j
                     d[i] = int(lcm(n1, n2))
                     break
-        else:
+        else: # The recurring decimals of numbers which are not coprime to 10
             n = i
             while n % 2 == 0:
                 n = n / 2
@@ -99,22 +100,5 @@ for i in range(6, 1000):
                 n = n / 5
             d[i] = d[int(n)]
 
-#print(list(d.values()).index(max(d.values())) + 1)
+print(list(d.values()).index(max(d.values())) + 1)
 
-
-def sieve(n):
-    is_prime1 = [True] * n
-    is_prime1[0] = False
-    is_prime1[1] = False
-    for i in range(2, int(n ** 0.5 + 1)):
-        index = i * 2
-        while index < n:
-            is_prime1[index] = False
-            index = index + i
-    prime = []
-    for i in range(n):
-        if is_prime1[i] == True:
-            prime.append(i)
-    return prime
-
-print(sieve(100))
